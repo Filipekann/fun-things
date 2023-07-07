4799fef6a0ed07e32c5834d41f34c5318a99be4d
Filip <filek275@student.liu.se>
2023-07-07 15:45:04 +0200

    Klar med lol_overlay losing_spree.py

--- lol_overlay/losing_spree.py
+++ lol_overlay/losing_spree.py
@@ -1,78 +1,255 @@
+import requests
+import json
+import itertools
 import time
-from riotwatcher import LolWatcher
+import os
 
-#Psudo kod:
-"""
-Vi kollar först vem är spelaren.
-Vilka i hens game är med?
-Gör en for loop som går igenom varje spelare förutom sig själv
-    I loopen kallar vi på en funktionen suger du
-    resultatet från du suger kopplas till spelarens namn via en dict
+API_KEY = 'RGAPI-ab9db0af-b659-48a4-91ee-b19973008d7c'
 
-print dict eller lägg dict i en txt fil som vi kan jobba med sen.
-    
+SUMMONER_NAME = 'phoneymacaroni'
+#NoosaMaan
+#RomnarN
+#Yun
+#Browntissue69
+#God Complexes
+#1T1T1T1T1T1
+#tomseli
+#BobbyNugget
+#phoneymacaroni
 
-def suger du(spelare):
-    if sats om spelaren har förlorat flera games i rad:
-        return ja han suger.
-    else 
-        return nä han e bra.
+# Constants for rate limiting
+REQUESTS_PER_SECOND = 20
+REQUESTS_PER_MINUTE = 100
+
+# Global variables for rate limiting
+request_counter = 0
+request_timestamps = []
 
 
-"""
+def main():
+    print(f"Getting players in game with {SUMMONER_NAME}:")
+    players = get_players_in_game()
+    print("Done\n")
+    """
+    print("Calculating win rates")
+    players_with_win_rates = calculate_win_rates(players)
+    print("Done\n")
+    """
+    print("Calculating losing spree")
+    losing_spree =calculate_losing_spree(players)
+    print("Done\n")
 
-# Replace 'YOUR_API_KEY' with your actual API key
-api_key = 'RGAPI-6bd202f1-ab1a-4621-af66-aa559db331f8'
-region = 'euw1'  # e.g., 'na1', 'euw1', 'kr', etc.
+    folder_name = "dict_txt"
 
-def get_losing_spree_players(summoner_name):
-    watcher = LolWatcher(api_key)
-    
-    try:
-        # Retrieve summoner details
-        summoner = watcher.summoner.by_name(region, summoner_name)
-        
-        # Retrieve current game details
-        current_game = watcher.spectator.by_summoner(region, summoner['id'])
-        
-        # Retrieve match history for each player in the current game
-        match_history = {}
-        for participant in current_game['participants']:
-            summoner_id = participant['summonerId']
-            match_history[summoner_id] = watcher.match.matchlist_by_account(region, summoner_id)
-        
-        # Process match history to identify players on a losing spree
-        losing_spree_players = []
-        for summoner_id, matches in match_history.items():
-            lost_games_in_a_row = 0
-            for match in matches['matches']:
-                match_details = watcher.match.by_id(region, match['gameId'])
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+        print(f"Created folder: {folder_name}")
+    else:
+        print(f"Folder {folder_name} already exists. Skipping folder creation.\n")
+
+    current_time = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+    file_name = f"{current_time}.txt"
+    file_path = os.path.join(folder_name, file_name)
+
+    with open(file_path, "w") as file:
+        json.dump(losing_spree, file, indent=4)
+
+    print(f"Created file: {file_path} with the players_with_win_rates dictionary.")
+
+
+
+def make_request(url):
+    global request_counter, request_timestamps
+
+    # Check rate limits
+    current_timestamp = time.time()
+    request_timestamps = [ts for ts in request_timestamps if ts > current_timestamp - 60]
+
+    if len(request_timestamps) >= REQUESTS_PER_MINUTE:
+        # Maximum requests per minute reached, wait for rate limit reset
+        sleep_time = max(request_timestamps[0] + 60 - current_timestamp, 0)
+        time.sleep(sleep_time)
+        request_timestamps = []
+
+    if request_counter >= REQUESTS_PER_SECOND:
+        # Maximum requests per second reached, wait for rate limit reset
+        sleep_time = max(request_timestamps[0] + 1 - current_timestamp, 0)
+        time.sleep(sleep_time)
+        request_counter = 0
+        request_timestamps = []
+
+    # Make the API request
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # Update rate limiting variables
+        request_counter += 1
+        request_timestamps.append(time.time())
+
+        return json.loads(response.text)
+    else:
+        error_message = json.loads(response.text)
+        raise Exception(f"API request failed: {error_message}")
+
+
+
+def get_players_in_game():
+    REGION = 'euw1'
+    base_url = f'https://{REGION}.api.riotgames.com'
+
+    endpoint = f'/lol/summoner/v4/summoners/by-name/{SUMMONER_NAME}'
+    url = base_url + endpoint + f'?api_key={API_KEY}'
+    data = make_request(url)
+
+    id = data['id']
+    puuid = data['puuid']
+    accountid = data['accountId']
+
+    endpoint = f'/lol/spectator/v4/active-games/by-summoner/{id}'
+    url = base_url + endpoint + f'?api_key={API_KEY}'
+    data = make_request(url)
+
+    participants = data['participants']
+    player_names = {}
+
+    for participant in participants:
+        summoner_name = participant['summonerName']
+        endpoint = f'/lol/summoner/v4/summoners/by-name/{summoner_name}'
+        url = base_url + endpoint + f'?api_key={API_KEY}'
+        data = make_request(url)
+
+        id = data['id']
+        accountId = data['accountId']
+        puuid = data['puuid']
+        name = data["name"]
+
+        player_names[name] = {
+            'id': id,
+            'accountId': accountId,
+            'puuid': puuid,
+            'teamId': participant['teamId'],
+            'championId': participant['championId']
+        }
+
+    return player_names
+
+
+
+def calculate_win_rates(players):
+    REGION = 'EUROPE'
+    base_url = f'https://{REGION}.api.riotgames.com'
+
+    with requests.Session() as session:
+        for player_name, player_info in players.items():
+            id = player_info['id']
+            puuid = player_info['puuid']
+            accountId = player_info['accountId']
+
+            endpoint = f'/lol/match/v5/matches/by-puuid/{puuid}/ids'
+            url = base_url + endpoint + f'?api_key={API_KEY}'
+            data = make_request(url)
+
+            matches = list(itertools.islice(data, 5))  # Limit to 5 matches
+
+            wins = 0
+            losses = 0
+
+            for match_id in matches:
+                endpoint = f'/lol/match/v5/matches/{match_id}'
+                url = base_url + endpoint + f'?api_key={API_KEY}'
+                match_data = make_request(url)
+
+                participant_identities = match_data['info']['participants']
+                participant_stats = match_data['info']['participants']
+
                 participant_id = None
-                for participant_identity in match_details['participantIdentities']:
-                    if participant_identity['player']['accountId'] == summoner_id:
-                        participant_id = participant_identity['participantId']
+                for identity in participant_identities:
+                    if identity['summonerId'] == id:
+                        participant_id = identity['participantId']
                         break
-                
-                participant_details = match_details['participants'][participant_id - 1]
-                if participant_details['stats']['win'] is False:
-                    lost_games_in_a_row += 1
-                else:
-                    lost_games_in_a_row = 0
-                    
-                if lost_games_in_a_row > 5:
-                    losing_spree_players.append(participant_details['summonerName'])
-        
-        return losing_spree_players
-    
-    except Exception as e:
-        print(f"Error: {e}")
-        return []
 
-# Replace 'YOUR_SUMMONER_NAME' with the summoner name you want to check
-summoner_name = 'phoneymacaroni'
+                for stat in participant_stats:
+                    if stat['participantId'] == participant_id:
+                        if stat['win']:
+                            wins += 1
+                        else:
+                            losses += 1
 
-losing_spree_players = get_losing_spree_players(summoner_name)
-if losing_spree_players:
-    print(f"Players on a losing spree greater than 5 games in a row: {', '.join(losing_spree_players)}")
-else:
-    print("No players found on a losing spree greater than 5 games in a row.")
+                time.sleep(0.5)  # Pause for 0.5 seconds between each match request
+
+            total_games = wins + losses
+            win_rate = (wins / total_games) * 100 if total_games > 0 else 0
+            players[player_name]['win_rate'] = win_rate
+
+    return players
+
+
+
+def calculate_losing_spree(players):
+    REGION = 'EUROPE'
+    base_url = f'https://{REGION}.api.riotgames.com'
+
+    with requests.Session() as session:
+        for player_name, player_info in players.items():
+            id = player_info['id']
+            puuid = player_info['puuid']
+            accountId = player_info['accountId']
+
+            endpoint = f'/lol/match/v5/matches/by-puuid/{puuid}/ids'
+            url = base_url + endpoint + f'?api_key={API_KEY}'
+            data = make_request(url)
+
+            matches = list(itertools.islice(data, 7))  # Limit 
+
+            current_streak = 0
+            win_streak = 0
+            loss_streak = 0
+
+            for match_id in matches:
+                endpoint = f'/lol/match/v5/matches/{match_id}'
+                url = base_url + endpoint + f'?api_key={API_KEY}'
+                match_data = make_request(url)
+
+                participant_identities = match_data['info']['participants']
+                participant_stats = match_data['info']['participants']
+
+                participant_id = None
+                for identity in participant_identities:
+                    if identity['summonerId'] == id:
+                        participant_id = identity['participantId']
+                        break
+
+                for stat in participant_stats:
+                    if stat['participantId'] == participant_id:
+                        if stat['win'] == True:
+                            if win_streak == current_streak:
+                                current_streak += 1
+                                win_streak = max(win_streak, current_streak)
+                                loss_streak = 0
+                            else:
+                                break
+                        elif stat['win'] == False:
+                            if loss_streak == current_streak:
+                                current_streak -= 1
+                                loss_streak = max(loss_streak, abs(current_streak))
+                                win_streak = 0
+                            else:
+                                break
+                        else:
+                            # Handle other result types (e.g., draw, tie) if applicable
+                            pass
+
+            if win_streak > 0:
+                players[player_name]['win_streak'] = win_streak
+            elif loss_streak > 0:
+                players[player_name]['loss_streak'] = loss_streak
+            else:
+                print("You are not currently on a winning or losing streak.")
+            
+            time.sleep(0.5)  # Pause for 0.5 seconds between each match request
+
+    return players
+
+
+
+main()
